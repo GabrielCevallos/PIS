5e9ab0a0bfd43f79efde0ea0ece3488f4a769396
Aris <ariana.s.sarango@unl.edu.ec>
2024-06-23 23:06:45 -0500

    Update Code

--- PIS/BrazoRobotico/apps.py
+++ PIS/BrazoRobotico/apps.py
@@ -4,3 +4,62 @@ from django.apps import AppConfig
 class BrazoroboticoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'BrazoRobotico'
+
+#Aris
+
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(PIS)
+app.secret_key = 'Pancho123'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/registro', methods=['GET', 'POST'])
+def registro():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User(username=username, email=email, password=password)
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Registrado exitosamente!')
+        return redirect(url_for('login'))
+
+    return render_template('registro.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            flash('Inicio de sesión exitoso!')
+            return redirect(url_for('index'))
+        else:
+            flash('Nombre de usuario o contraseña incorrectos.')
+
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
